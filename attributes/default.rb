befed3be0e2a70900368d8b597be33b49f331ca8
arjun-hariharan <harjun1601@gmail.com>
2014-10-09 07:29:44 +0530

    place holders

---
+++ attributes/default.rb
@@ -0,0 +1,13 @@
+# template
+
+# default['netapp']['url'] = "http://root:secret@example.com"
+
+#### or
+
+# default['netapp']['https'] = false
+# default['netapp']['user'] = 'root'
+# default['netapp']['password'] = 'secret'
+# default['netapp']['fqdn'] = 'example.com'
+
+############ timeout ######################
+# default['netapp']['api']['timeout'] = 60000
