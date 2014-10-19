fd3d90cd7395a5df81c3cae6f48ea1965f960d6e
arjun-hariharan <harjun1601@gmail.com>
2014-10-19 21:56:39 +0530

    Minor code modifications

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,5 +8,9 @@
 # default['netapp']['fqdn'] = 'example.com'
 # default['netapp']['basic_auth'] = true
 
+########### Web Proxy Port (optional)#################
+
+# default['netapp']['port'] = 8080
+
 ############ timeout (optional) ######################
 # default['netapp']['api']['timeout'] = 60000
--- libraries/helper.rb
+++ libraries/helper.rb
@@ -1,11 +1,9 @@
 module NetAppEHelper
   def url
-    port = node['netapp']['port'] unless node['netapp']['port'].nil?
-
     if node['netapp']['https']
-      "https://#{node['netapp']['fqdn']}:#{port.nil? ? 8443 :  port}"
+      "https://#{node['netapp']['fqdn']}:#{node['netapp']['port'].nil? ? 8443 :  node['netapp']['port']}"
     else
-      "http://#{node['netapp']['fqdn']}:#{port.nil? ? 8080 :  port}"
+      "http://#{node['netapp']['fqdn']}:#{node['netapp']['port'].nil? ? 8080 :  node['netapp']['port']}"
     end
   end
 
@@ -15,10 +13,15 @@ module NetAppEHelper
     fail unless node['netapp']['password'].class == String
     fail unless node['netapp']['fqdn'].class == String
     fail unless node['netapp']['basic_auth'] == true || node['netapp']['basic_auth'] == false
+  rescue
+    Chef::Log.error('One or more mandatory parameters are not provided or are set incorrectly')
+    exit
   end
 
   def netapp_api_create
     # Validate the mandatory parameters and create a netapp api object
+    validate_node_attributes
+
     begin
       # Check if user has given a timeout value
       timeout_set = true if node['netapp']['api']['timeout']
@@ -26,16 +29,10 @@ module NetAppEHelper
       timeout_set = false
     end
 
-    begin
-      validate_node_attributes
-      if timeout_set
-        NetApp::ESeries::Api.new(node['netapp']['user'], node['netapp']['password'], url, node['netapp']['basic_auth'], node['netapp']['api']['timeout']) if node['netap']['api']['timeout']
-      else
-        NetApp::ESeries::Api.new(node['netapp']['user'], node['netapp']['password'], url, node['netapp']['basic_auth'])
-      end
-    rescue
-      Chef::Log.error('One or more mandatory parameters are not provided or are set incorrectly')
-      exit
+    if timeout_set
+      NetApp::ESeries::Api.new(node['netapp']['user'], node['netapp']['password'], url, node['netapp']['basic_auth'], node['netapp']['api']['timeout']) if node['netapp']['api']['timeout']
+    else
+      NetApp::ESeries::Api.new(node['netapp']['user'], node['netapp']['password'], url, node['netapp']['basic_auth'])
     end
   end
 end
