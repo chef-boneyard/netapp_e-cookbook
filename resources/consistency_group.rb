045168d253f404604ffe8a61f7386bc73c4f77a0
Mahendraes <mahendra.deore@msystechnologies.com>
2015-11-19 17:33:05 +0530

    Added Consistency Group Resource

---
+++ resources/consistency_group.rb
@@ -0,0 +1,24 @@
+#
+# cookbook Name:: netapp_e
+# Resource:: volume
+#
+# Copyright 2014, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+actions :create, :delete
+default_action :create
+
+attribute :name, kind_of: String, required: true, name_attribute: true
+attribute :storage_system, kind_of: String, required: true
