f66086a9889c667c04eb4462403590ac1fdcf93d
gaurav6351 <gaurav.harsola@msystechnologies.com>
2015-11-24 18:03:36 +0530

    Fixed typo

--- providers/mirror_group.rb
+++ providers/mirror_group.rb
@@ -39,7 +39,7 @@ action :delete do
   netapp_api = netapp_api_create
 
   netapp_api.login unless node['netapp']['basic_auth']
-  resource_update_status = netapp_api.delete_storage_pool(new_resource.storage_system, new_resource.name)
+  resource_update_status = netapp_api.delete_mirror_group(new_resource.storage_system, new_resource.name)
   netapp_api.logout unless node['netapp']['basic_auth']
 
   new_resource.updated_by_last_action(true) if resource_update_status
